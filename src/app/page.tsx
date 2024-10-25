fb1f32b391d450e1b9bd039437a8a6f95b368802
Dana Rocha <hello@danarocha.com>
2024-10-25 12:56:17 +0300

    fix deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
+import { FloatingDock } from '@/components/ui/floating-dock';
 import { Header } from '@/components/ui/header';
 import { TabataTimer } from "@/components/ui/timer";
+import { GitHubLogoIcon,  } from '@radix-ui/react-icons';
+import { X, Mail } from "lucide-react";
 
 export default function Home() {
 
@@ -10,6 +13,40 @@ export default function Home() {
       <main className="w-full h-[calc(100vh-80px)] flex justify-center items-center relative">
         <TabataTimer />
       </main>
+
+      <footer className="relative">
+        <FloatingDock
+          items={[
+            {
+              title: "GitHub",
+              icon: (
+                <GitHubLogoIcon className="h-full w-full text-text-header/50 dark:text-text-header" />
+              ),
+              href: "#",
+            },
+            {
+              title: "X (Twitter)",
+              icon: (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  className="fill-text-header/50 dark:fill-text-header"
+                >
+                  <path d="M10.4883 14.651L15.25 21H22.25L14.3917 10.5223L20.9308 3H18.2808L13.1643 8.88578L8.75 3H1.75L9.26086 13.0145L2.31915 21H4.96917L10.4883 14.651ZM16.25 19L5.75 5H7.75L18.25 19H16.25Z"></path>
+                </svg>
+              ),
+              href: "https://x.com/danarocha_",
+            },
+            {
+              title: "Say hi 👋!",
+              icon: (
+                <Mail className="h-full w-full text-text-header/50 dark:text-text-header" />
+              ),
+              href: "mailto:hello@danarocha.com",
+            },
+          ]}
+        />
+      </footer>
     </div>
   );
 }
---
+++ src/components/ui/floating-dock.tsx
@@ -0,0 +1,142 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import {
+  AnimatePresence,
+  MotionValue,
+  motion,
+  useMotionValue,
+  useSpring,
+  useTransform,
+} from "framer-motion";
+import Link from "next/link";
+import { useRef, useState } from "react";
+
+export const FloatingDock = ({
+  items,
+  desktopClassName,
+}: {
+  items: { title: string; icon: React.ReactNode; href: string }[];
+  desktopClassName?: string;
+}) => {
+  return (
+    <div className="fixed bottom-6 flex justify-center w-full">
+      <FloatingDockDesktop items={items} className={desktopClassName} />
+    </div>
+  );
+};
+
+const FloatingDockDesktop = ({
+  items,
+  className,
+}: {
+  items: { title: string; icon: React.ReactNode; href: string }[];
+  className?: string;
+}) => {
+  const mouseX = useMotionValue(Infinity);
+  return (
+    <motion.div
+      onMouseMove={(e) => mouseX.set(e.pageX)}
+      onMouseLeave={() => mouseX.set(Infinity)}
+      className={cn(
+        "mx-auto text-xs text-text-header/40 hidden md:flex h-8 gap-4 items-center rounded-2xl bg-white dark:bg-muted px-2 py-2",
+        className
+      )}
+    >
+      Made by @danarocha
+    <span className='w-px h-2 bg-secondary'></span>
+      {items.map((item) => (
+        <IconContainer mouseX={mouseX} key={item.title} {...item} />
+      ))}
+    </motion.div>
+  );
+};
+
+function IconContainer({
+  mouseX,
+  title,
+  icon,
+  href,
+}: {
+  mouseX: MotionValue;
+  title: string;
+  icon: React.ReactNode;
+  href: string;
+}) {
+  const ref = useRef<HTMLDivElement>(null);
+
+  const distance = useTransform(mouseX, (val) => {
+    const bounds = ref.current?.getBoundingClientRect() ?? { x: 0, width: 0 };
+
+    return val - bounds.x - bounds.width / 2;
+  });
+
+  const widthTransform = useTransform(distance, [-200, 0, 200], [18, 28, 18]);
+  const heightTransform = useTransform(distance, [-200, 0, 200], [18, 28, 18]);
+
+  const widthTransformIcon = useTransform(
+    distance,
+    [-200, 0, 200],
+    [18, 40, 18]
+  );
+  const heightTransformIcon = useTransform(
+    distance,
+    [-200, 0, 200],
+    [18, 40, 18]
+  );
+
+  const width = useSpring(widthTransform, {
+    mass: 0.1,
+    stiffness: 150,
+    damping: 12,
+  });
+  const height = useSpring(heightTransform, {
+    mass: 0.1,
+    stiffness: 150,
+    damping: 12,
+  });
+
+  const widthIcon = useSpring(widthTransformIcon, {
+    mass: 0.1,
+    stiffness: 150,
+    damping: 12,
+  });
+  const heightIcon = useSpring(heightTransformIcon, {
+    mass: 0.1,
+    stiffness: 150,
+    damping: 12,
+  });
+
+  const [hovered, setHovered] = useState(false);
+
+  return (
+    <Link href={href}>
+      <motion.div
+        ref={ref}
+        style={{ width, height }}
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+        className="h-8 w-8 rounded-full flex items-center justify-center relative"
+      >
+        <AnimatePresence>
+          {hovered && (
+            <motion.div
+              initial={{ opacity: 0, y: 10, x: "-50%" }}
+              animate={{ opacity: 1, y: 0, x: "-50%" }}
+              exit={{ opacity: 0, y: 2, x: "-50%" }}
+              className="px-2 py-0.5 whitespace-pre rounded-md bg-input text-card-foreground absolute left-1/2 -translate-x-1/2 -top-8 w-fit text-xs"
+            >
+              {title}
+            </motion.div>
+          )}
+        </AnimatePresence>
+        <motion.div
+          style={{ width: widthIcon, height: heightIcon }}
+          className="flex items-center justify-center"
+        >
+          {icon}
+        </motion.div>
+      </motion.div>
+    </Link>
+  );
+}
